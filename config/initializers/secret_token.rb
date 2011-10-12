95cf973b0aa3f61ada42f896efc9a5dfc852026e
Robert Jenkins <rpjenkins@valdosta.edu>
2011-10-12 13:24:22 -0400

    first commit for calendar view

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CalendarView::Application.config.secret_token = 'b0b4674377bc7d6c009e09f8b083eee297de5b0fd722c4ee9a7566c5a145ff26a336e5bc00fdfd6390cd495dea6485a7f7c33b1dcbe12217c6a6b0d977dbbb40'
